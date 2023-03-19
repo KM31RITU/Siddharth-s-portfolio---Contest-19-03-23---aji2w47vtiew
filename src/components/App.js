4055eaeb846afddfc2839fe19bacada609fbfdc2
KUMARI RITU <120951254+KM31RITU@users.noreply.github.com>
2023-03-19 13:57:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import '../styles/App.css';
 // import { BrowserRouter as Router, Route,Link,Switch } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from 'react-router-dom';
-import Info from './Info';
-import Blog from './Blog';
-import Home from './Home';
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from "react-router-dom";
+import Info from "./Info";
+import Blog from "./Blog";
+import Home from "./Home";
 const App = () => {
 
   return (
@@ -15,10 +15,10 @@ const App = () => {
           <Route path="/" exact>
             <Home />
           </Route>
-          <route path='/blog' exact>
+          <route path="/blog" exact>
             <Blog />
           </route>
-          <route path='/info' exact>
+          <route path="/info" exact>
             <Info />
           </route>
         </Switch>
