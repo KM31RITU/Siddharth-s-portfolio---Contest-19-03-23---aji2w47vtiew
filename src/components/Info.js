56531c1f03a8b7ef6dbbdbc495e1f217d51f40ae
KUMARI RITU <120951254+KM31RITU@users.noreply.github.com>
2023-03-19 14:07:41 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,14 +6,16 @@ const Info = () => {
 
     return (
         <div id="info-page">
-
-
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
+        <Link to="/blog">
              <button id="info-to-blog"> Go to Blog</button>
+        </Link>
+        <Link to="/">
             <button id="info-to-home"> Go to Home</button>
+        </Link>
         </div>
-    )
-}
+    );
+};
 export default Info
